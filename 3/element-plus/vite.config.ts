0035978c4cf147b17de581fe531dd2350372c575
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-08-31 20:41:41 +0200

    build: element plus works

--- 3/element-plus/vite.config.ts
+++ 3/element-plus/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
+import Unocss from 'unocss/vite'
+import {
+  presetAttributify,
+  presetIcons,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+const pathSrc = path.resolve(__dirname, 'src')
+
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      '~/': `${pathSrc}/`,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/element/index.scss" as *;`,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+
+    AutoImport({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass',
+        }),
+      ],
+      dts: 'src/components.d.ts',
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
