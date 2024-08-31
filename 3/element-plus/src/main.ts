2a86cdd2a78aefba7c2e3b992f0247ce1e535def
Dmytro Tsysar <1106994+idmytro@users.noreply.github.com>
2024-08-31 20:58:47 +0200

    element-plus

--- 3/element-plus/src/main.ts
+++ 3/element-plus/src/main.ts
@@ -1,7 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import 'element-plus/theme-chalk/src/message.scss'
+// import "~/styles/element/index.scss";
 
-createApp(App).mount('#app')
+// import ElementPlus from "element-plus";
+// import all element css, uncommented next line
+// import "element-plus/dist/index.css";
+
+// or use cdn, uncomment cdn link in `index.html`
+
+import "~/styles/index.scss";
+import "uno.css";
+
+// If you want to use ElMessage, import it.
+import "element-plus/theme-chalk/src/message.scss";
+
+const app = createApp(App);
+// app.use(ElementPlus);
+app.mount("#app");
--- 3/element-plus/vite.config.ts
+++ 3/element-plus/vite.config.ts
@@ -1,7 +1,7 @@
-import path from 'node:path'
-import vue from '@vitejs/plugin-vue'
+import path from 'path'
 import { defineConfig } from 'vite'
-import AutoImport from 'unplugin-auto-import/vite'
+import vue from '@vitejs/plugin-vue'
+
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
@@ -16,6 +16,7 @@ import {
 
 const pathSrc = path.resolve(__dirname, 'src')
 
+// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -31,8 +32,11 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-
-    AutoImport({
+    Components({
+      // allow auto load markdown components under `./src/components/`
+      extensions: ['vue', 'md'],
+      // allow auto import and register components used in markdown
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       resolvers: [
         ElementPlusResolver({
           importStyle: 'sass',
@@ -40,8 +44,22 @@ export default defineConfig({
       ],
       dts: 'src/components.d.ts',
     }),
-    Components({
-      resolvers: [ElementPlusResolver()],
+
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          scale: 1.2,
+          warn: true,
+        }),
+      ],
+      transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+      ]
     }),
   ],
 })
